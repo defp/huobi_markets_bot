a530baca578db72abfc93729bc2749e0b73ce670
ifsclimbing <ifsclimbing@gmail.com>
2018-01-07 13:39:11 +0800

    feat: ping api

--- main.go
+++ main.go
@@ -1,2 +1,97 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"bytes"
+	"compress/gzip"
+	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"strings"
+)
+
+var addr = flag.String("addr", "api.huobi.pro", "http service address")
+
+type Heartbeat struct {
+	Ping int64 `json:"ping"`
+}
+
+func unzip(data []byte) ([]byte, error) {
+	r, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(r)
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "wss", Host: *addr, Path: "/ws"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer c.Close()
+		defer close(done)
+		for {
+			_, data, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read error:", err)
+				return
+			}
+			jsonData, err := unzip(data)
+			text := string(jsonData)
+			log.Println("receive text: ", text)
+			if strings.Contains(text, "ping") {
+				rspText := strings.Replace(text, "ping", "pong", 1)
+				if err = c.WriteMessage(websocket.TextMessage, []byte(rspText)); err != nil {
+					log.Println(err)
+				}
+			} else {
+				//TODO
+			}
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case t := <-ticker.C:
+			log.Println(t.String())
+		case <-interrupt:
+			log.Println("interrupt")
+			// To cleanly close a connection, a client should send a close
+			// frame and wait for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			c.Close()
+			return
+		}
+	}
+}
